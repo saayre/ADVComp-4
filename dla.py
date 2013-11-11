4a15fdc3d8be7973406103410d3335f677d9d57e
saayre <glen.christenson@gmail.com>
2013-11-11 16:50:18 -0500

    still need to do box count

--- dla.py
+++ dla.py
@@ -2,85 +2,122 @@
 
 import numpy as num
 import random as ran
+import sys
 
 def check():
     chk = 0
 
-    if grid[ix+1, iy] == 1:
+    if grid[ (ix+1)%(side-1), iy] == 1:
         chk = 1
-    if grid[ix-1, iy] == 1:
+    if grid[ (ix-1)%(side-1), iy] == 1:
         chk = 1
-    if grid[ix, iy+1] == 1:
+    if grid[ix, (iy+1)%(side-1)] == 1:
         chk = 1
-    if grid[ix, iy-1] == 1:
+    if grid[ix, (iy-1)%(side-1)] == 1:
         chk = 1
 
     return chk
-
 
 def move(direction):
     global ix, iy
 
     if direction == 1:
         ix += 1
-    if direction == 1:
+    if direction == 2:
         ix -= 1
-    if direction == 1:
+    if direction == 3:
         iy += 1
-    if direction == 1:
-        ix -= 1
+    if direction == 4:
+        iy -= 1
 
-    ix %= side - 1
-    iy %= side - 1
+    ix %= side
+    iy %= side
 
 
 def check8():
-    return 0
+    chk = 0
 
+    if grid[ (ix+1)%(side-1), iy] == 1:
+        chk = 1
+    if grid[ (ix-1)%(side-1), iy] == 1:
+        chk = 1
+    if grid[ix, (iy+1)%(side-1)] == 1:
+        chk = 1
+    if grid[ix, (iy-1)%(side-1)] == 1:
+        chk = 1
 
+    if grid[ (ix+1)%(side-1), (iy+1)%(side-1)] == 1:
+        chk = 1
+    if grid[ (ix-1)%(side-1), (iy+1)%(side-1)] == 1:
+        chk = 1
+    if grid[ (ix+1)%(side-1), (iy-1)%(side-1)] == 1:
+        chk = 1
+    if grid[ (ix-1)%(side-1), (iy-1)%(side-1)] == 1:
+        chk = 1
 
+    return chk
 
 def move8(direction):
-    return 0
+    global ix, iy
+
+    if direction == 1 or direction == 5 or direction == 6:
+        ix += 1
+    if direction == 2 or direction == 7 or direction == 8:
+        ix -= 1
+    if direction == 3 or direction == 5 or direction == 7:
+        iy += 1
+    if direction == 4 or direction == 6 or direction == 8:
+        iy -= 1
+
+    ix %= side
+    iy %= side
 
 
-diag = 0
+if len(sys.argv) == 1:
+    print "dla N side diag"
+    exit()
 
 # switch on diagonals
-if diag == 1:
+if len(sys.argv) == 4:
     check = check8
     move = move8
 
 # initialize grid
-side = 512
+side = int(sys.argv[2])
 grid = num.zeros( (side, side), dtype=int)
 
 grid[side/2, side/2] = 1
 
 # walk in particles
-N = 100
+N = int(sys.argv[1])
 ran.seed(None)
 
 for n in range(N):
 
-    ix = ran.randint(0, side-1)
-    iy = ran.randint(0, side-1)
-
-    if ix > side/4 and ix < 3*side/4 and iy > side/4 and iy < 3*side/4:
-        continue
+    if ran.randint(0, 1) == 0:
+        ix = ran.randint(0, side-1)
+        iy = ran.randint(0, 1) * (side -1)
+        
+    else:
+        iy = ran.randint(0, side-1)
+        ix = ran.randint(0, 1) * (side - 1)
 
     if grid[ix, iy] == 1:
+        n -= 1
         continue
 
+    print n
     # move
     while check() == 0:
-        move(ran.randint(1, 4))
-        print ix, iy
+        if len(sys.argv) == 4:
+            move(ran.randint(1, 8))
+        else:
+            move(ran.randint(1, 4))
 
     grid[ix, iy] = 1
 
-with open("plotdla.dat", "w") as f:
-    for inds, val in enumerate(grid):
+with open("map.dat", "w") as f:
+    for y_ind, row in enumerate(grid):
+        for x_ind, val in enumerate(row):
 
-        f.write(inds)
-
+            f.write( '{0} {1} {2}\n'.format(x_ind, y_ind, val))
